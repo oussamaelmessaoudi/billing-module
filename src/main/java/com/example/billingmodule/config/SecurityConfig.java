b4200d4c2855afe06ef8853633171d690412ef83
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-09-21 22:11:15 +0100

    Disabling authentication and permit all HttpRequest from any source using SecurityFilterChain

--- src/main/java/com/example/billingmodule/config/SecurityConfig.java
+++ src/main/java/com/example/billingmodule/config/SecurityConfig.java
@@ -1,4 +1,17 @@
 package com.example.billingmodule.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(c -> c.disable()).build();
+    }
 }
